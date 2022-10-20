dbd95137805e86fd445a7acc4b455e8467351940
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2022-10-20 15:39:00 +0100

    responsive test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
-import background from './images/Courier_Mobmed.png';
-import Mobile from './components/Mobile';
-import Dash from './components/Dash';
+// import background from './images/Courier_Mobmed.png';
+// import Mobile from './components/Mobile';
+// import Dash from './components/Dash';
 
 function App() {
 
   return (
     <div className="App">
-    <Mobile></Mobile>
-    <Dash></Dash>
+    {/* <Mobile></Mobile> */}
+    {/* <Dash></Dash> */}
     
     <div id="Content">
 
       <div className='Logo'><i class="fa-solid fa-heart-pulse"></i></div>
+
         <div className='MyComponents'>
-          <div className='component-1'></div>
-          <div className='component-2'></div>
+          <div className='ComponentBox-1'><div className='component-1'></div></div>
+          <div className='ComponentBox-2'><div className='component-2'></div></div>
           <div className='Welcome'>
             <h1>MOBILE MEDICATION</h1>
             <p>
@@ -27,11 +28,12 @@ function App() {
                                     information on the right.
             </p>
           </div>
-          <div className='component-3'></div>
-          <div className='component-4'></div>
-          <div className='component-5'><div className='Links'><a href='#'><i class="fa-solid fa-user"></i></a><a href='#'><i class="fa-solid fa-list"></i></a><a href='#'><i class="fa-solid fa-folder"></i></a><a href='#'><i class="fa-sharp fa-solid fa-gear"></i></a></div></div>
+          <div className='ComponentBox-3'><div className='component-3'></div></div>
+          <div className='ComponentBox-4'><div className='component-4'></div></div>
+          <div className='ComponentBox-5'><div className='component-5'><div className='Links'><a href='#'><i class="fa-solid fa-user"></i></a><a href='#'><i class="fa-solid fa-list"></i></a><a href='#'><i class="fa-solid fa-folder"></i></a><a href='#'><i class="fa-sharp fa-solid fa-gear"></i></a></div></div></div>
         </div>
-      <div className='background'><div className='Blend'></div><img src={background}/></div>
+
+      {/* <div className='background'><div className='Blend'></div><img src={background}/></div> */}
       
       <div className='footer'>
           <div className='FooterIcons'><i class="fa-brands fa-facebook"></i><i class="fa-brands fa-twitter"></i><i class="fa-brands fa-instagram"></i></div>
