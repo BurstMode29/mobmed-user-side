66116a6e4e1a9b1c6163157146258300fa9d8a8c
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2022-10-21 15:04:56 +0100

    updated responsivness

--- src/App.js
+++ src/App.js
@@ -1,45 +1,68 @@
 import './App.css';
 // import background from './images/Courier_Mobmed.png';
-// import Mobile from './components/Mobile';
+import Mobile from './components/Mobile';
 import Dash from './components/Dash';
 
 function App() {
 
+  function MobileMenu() {
+    var m = document.getElementById("MobileMenu");
+    if (m.style.height === "0%") {
+      m.style.height = "100%";
+      document.getElementById("PP").style.left = "5px";
+      document.getElementById("DI").style.left = "0px";
+      document.getElementById("DC").style.left = "0px";
+      document.getElementById("DT").style.left = "0px";
+    } else {
+      m.style.height = "0%"
+      document.getElementById("PP").style.left = "-50px";
+      document.getElementById("DI").style.left = "-300px";
+      document.getElementById("DC").style.left = "-310px";
+      document.getElementById("DT").style.left = "-300px";
+    }
+  }
+
   return (
     <div className="App">
-    {/* <Mobile></Mobile> */}
-    <Dash></Dash>
-    
-    <div id="Content">
+      <Mobile></Mobile>
+      <Dash></Dash>
 
-      <div className='Logo'><i class="fa-solid fa-heart-pulse"></i></div>
+      <div id="Content">
+        <div>
 
-        <div className='MyComponents'>
-          <div className='ComponentBox-1'><div className='component-1'></div></div>
-          <div className='ComponentBox-2'><div className='component-2'></div></div>
-          <div className='Welcome'>
-            <h1>MOBILE MEDICATION</h1>
-            <p>
+          <div className='MobileMenuIcon' onClick={MobileMenu}><i class="fa-solid fa-bars"></i></div>
+          <div className='Logo'><i class="fa-solid fa-heart-pulse"></i></div>
+
+          <div className='MyComponents'>
+            <div className='ComponentBox-1'><div className='component-1'></div></div>
+            <div className='ComponentBox-2'><div className='component-2'></div></div>
+            <div className='Welcome'>
+              <h1>MOBILE MEDICATION</h1>
+
+              <div className='WelcomeText'>
+              <p>
                 Welcome user on this platform you can see all your requests and bookings
-                  see your reviews, stats and your work history by clicking the buttons.
+                see your reviews, stats and your work history by clicking the buttons.
 
-                          Keep a sharp eye open for any new notifications in
-                            the top right and check customer location and 
-                                    information on the right.
-            </p>
+                Keep a sharp eye open for any new notifications in
+                the top right and check customer location and
+                information on the right.
+              </p>
+              </div>
+            </div>
+            <div className='ComponentBox-3'><div className='component-3'></div></div>
+            <div className='ComponentBox-4'><div className='component-4'></div></div>
+            <div className='ComponentBox-5'><div className='component-5'><div className='Links'><a href='#'><i class="fa-solid fa-user"></i></a><a href='#'><i class="fa-solid fa-list"></i></a><a href='#'><i class="fa-solid fa-folder"></i></a><a href='#'><i class="fa-sharp fa-solid fa-gear"></i></a></div></div></div>
           </div>
-          <div className='ComponentBox-3'><div className='component-3'></div></div>
-          <div className='ComponentBox-4'><div className='component-4'></div></div>
-          <div className='ComponentBox-5'><div className='component-5'><div className='Links'><a href='#'><i class="fa-solid fa-user"></i></a><a href='#'><i class="fa-solid fa-list"></i></a><a href='#'><i class="fa-solid fa-folder"></i></a><a href='#'><i class="fa-sharp fa-solid fa-gear"></i></a></div></div></div>
-        </div>
 
-      {/* <div className='background'><div className='Blend'></div><img src={background}/></div> */}
-      
-      <div className='footer'>
-          <div className='FooterIcons'><i class="fa-brands fa-facebook"></i><i class="fa-brands fa-twitter"></i><i class="fa-brands fa-instagram"></i></div>
-          <div className='FooterText'>© MOBMED | MOBILE MEDICATION</div>
+          {/* <div className='background'><div className='Blend'></div><img src={background}/></div> */}
+
+          <div className='footer'>
+            <div className='FooterIcons'><i class="fa-brands fa-facebook"></i><i class="fa-brands fa-twitter"></i><i class="fa-brands fa-instagram"></i></div>
+            <div className='FooterText'>© MOBMED | MOBILE MEDICATION</div>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -5,47 +5,29 @@ const Mobile = () => {
 
   // Mobile Menu
 
-  function MobileMenu() {
-    var m = document.getElementById("MobileMenu");
-    if (m.style.height === "0%") {
-      m.style.height = "100%";
-      document.getElementById("PP").style.left = "5px";
-      document.getElementById("DI").style.left = "0px";
-      document.getElementById("DC").style.left = "0px";
-      document.getElementById("DT").style.left = "0px";
-    } else {
-      m.style.height = "0%"
-      document.getElementById("PP").style.left = "-50px";
-      document.getElementById("DI").style.left = "-300px";
-      document.getElementById("DC").style.left = "-310px";
-      document.getElementById("DT").style.left = "-300px";
-    }
-  }
-
   // Mobile Menu
 
   return (
     <div>
 
-      <div className='MobileMenuIcon' onClick={MobileMenu}><i class="fa-solid fa-bars"></i></div>
       <div className='MobileMenu' id='MobileMenu'>
-        <div className='ProfilePhoto' id='PP'></div>
-        <div className='DashIcons' id='DI'>
+        {/* <div className='ProfilePhoto' id='PP'></div> */}
+        {/* <div className='DashIcons' id='DI'>
           <div className='MobileBell'><i class="fa-sharp fa-solid fa-bell"></i></div>
           <div className='MobileSettings'><i class="fa-solid fa-gear"></i></div>
           <div className='MobileMessaging'><i class="fa-sharp fa-solid fa-comment"></i></div>
-        </div>
+        </div> */}
 
-        <div className='DashCards' id='DC'>
+        {/* <div className='DashCards' id='DC'>
           <div className='DashHeader'>DashBoard</div>
           <div className='Cards'>
             <div className='Revenue'></div>
             <div className='Sales'></div>
             <div className='Cost'></div>
           </div>
-        </div>
+        </div> */}
 
-        <div className='DashTable' id='DT'>
+        {/* <div className='DashTable' id='DT'>
           <div className='TableHeader'>Recent Requests</div>
           <table className='MobileTable'>
             <tr>
@@ -61,7 +43,7 @@ const Mobile = () => {
               <td>Approved</td>
             </tr>
           </table>
-        </div>
+        </div> */}
 
       </div>
 
